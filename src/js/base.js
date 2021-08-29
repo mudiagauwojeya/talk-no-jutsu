f2b8071583907268abdada13970c9edc7a448225
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-08-29 19:25:40 +0100

    :construction: Declare the fetch function

--- src/js/base.js
+++ src/js/base.js
@@ -1,3 +1,12 @@
 export const manga = {
     quotes = {}
 }
+
+
+export const getQuotes = async (query) => {
+    try {
+        const quotesData = await fetch();
+    } catch (error) {
+        
+    }
+}
