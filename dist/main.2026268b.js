af1e4a6f4fdd84fef7dc8f4d540fb66abef6067e
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-22 19:11:15 +0100

    :lipstick: Update the bg-image of the content section

--- dist/main.2026268b.js
+++ dist/main.2026268b.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./..\\..\\assets\\manga_shop.jpg":[["manga_shop.a8f0f4eb.jpg","assets/manga_shop.jpg"],"assets/manga_shop.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./..\\..\\assets\\manga_shop_mod.jpg":[["manga_shop_mod.e9732764.jpg","assets/manga_shop_mod.jpg"],"assets/manga_shop_mod.jpg"],"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
