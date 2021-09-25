1d24b53b50256acce807fc4cb1dc22d053dfc9bc
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-25 20:52:01 +0100

    :zap: Add a helper function to removeActiveClass

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,1 +1,9 @@
 //Add helper functions for the project here
+
+//helper function to remove active class from selected element
+const removeActiveClass = (selector) => {
+	const elements = document.querySelectorAll(selector);
+	elements.forEach((element) => {
+		element.classList.remove("active");
+	});
+};
