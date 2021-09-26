076491c5312305150b44c4b335437df8e45c59ae
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-09-26 20:28:05 +0100

    :construction: Call getRandomQuote

--- dist/app.77c12427.js
+++ dist/app.77c12427.js
@@ -198,6 +198,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var ui = new _UI.default();
 ui.navigate();
+ui.getRandomQuote("random");
 },{"./UI/UI":"src/js/UI/UI.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,4 @@ import UI from "./UI/UI";
 const ui = new UI();
 
 ui.navigate();
+ui.getRandomQuote("random");
