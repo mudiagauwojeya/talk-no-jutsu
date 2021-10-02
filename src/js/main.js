179156631b4fb5f8e033e0bff791e9a3a3a290f0
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-02 19:32:45 +0100

    :construction: Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,4 @@
-import { API_ENDPOINT } from "./config.js";
-
 export const manga = {
 	quotes: [],
 	favorites: [],
-};
-
-export const getQuotes = async (query) => {
-	try {
-		const response = await fetch(`${API_ENDPOINT}${query}`);
-		const quotesData = await response.json();
-	} catch (error) {}
 };
