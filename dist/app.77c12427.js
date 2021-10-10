8e4930559ca7f6f1214c0835584426b602bc488f
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-10 20:11:08 +0100

    :tada: Begin new development session

--- dist/app.77c12427.js
+++ dist/app.77c12427.js
@@ -1122,7 +1122,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50843" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50873" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
