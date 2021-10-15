d6d289474248b116a9e16e86d1a7ff16db272853
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-10-15 20:36:44 +0100

    :recycle: Refactor the spinner class

--- src/js/components/spinner.js
+++ src/js/components/spinner.js
@@ -5,22 +5,17 @@ class Spinner {
 	static toggleModalAndSpinner(toggle = true) {
 		this.template = document.getElementById("template");
 		this.templateContent = document.importNode(this.template.content, true);
+		this.spinner = this.templateContent.querySelector(".spinner");
 
 		if (!toggle) {
-			this.modal = this.templateContent.querySelector(".modal");
-			this.spinner = this.templateContent.querySelector(".spinner");
-			this.modal.classList.remove("active");
 			this.spinner.classList.remove("active");
 			return;
 		}
-		this.modal = this.templateContent.querySelector(".modal");
-		this.spinner = this.templateContent.querySelector(".spinner");
-		this.modal.classList.add("active");
+
 		this.spinner.classList.add("active");
 		document.body.insertAdjacentElement("afterbegin", this.spinner);
 		//REVIEW: add the top property to control the scroll
 		this.spinner.scrollIntoView({ behavior: "smooth" });
-		document.body.insertAdjacentElement("afterbegin", this.modal);
 	}
 
 	hide() {
