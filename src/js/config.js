be95217d8dcd0571e6fe3c032cd77f31d5880176
Mudiaga Moody Uwojeya <moody.mudiaga@outlook.com>
2021-11-01 19:42:00 +0100

    :construction: Reconstruct the api string

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 //Add the configuration files for the project here
-export const API_ENDPOINT = `https://animechan.vercel.app/api/random`;
+export const API_ENDPOINT = "https://animechan.vercel.app/api/random";
 
 export const API_CHARACTER_NAME = (name) => {
 	return `https://animechan.vercel.app/api/quotes/character?name=${name}`;
